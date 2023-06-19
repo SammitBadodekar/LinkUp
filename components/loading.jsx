2dc667170dc8112cf7e46223589b2d9c8beea7f5
sammit badodekar <badodekarsammit@gmail.com>
2023-06-19 14:55:11 +0530

    fixed module-not-found

--- components/loading.jsx
+++ components/loading.jsx
@@ -1,11 +1,8 @@
 import { HashLoader } from "react-spinners";
-import logo from "../public/imgs/li_nkup-removebg-preview.png";
 
 const loading = () => {
   return (
-    <div className=" dark:bg-darkTheme flex justify-center items-center w-screen h-screen">
-      <img src={logo} alt="" />
-    </div>
+    <div className=" dark:bg-darkTheme flex justify-center items-center w-screen h-screen"></div>
   );
 };
 export default loading;
