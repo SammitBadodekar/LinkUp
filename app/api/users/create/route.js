6b0608a2c9aef1b67a2100f3dd1d997f482cb048
sammit badodekar <badodekarsammit@gmail.com>
2023-06-23 13:04:08 +0530

    test2: DB connection

--- app/api/users/create/route.js
+++ app/api/users/create/route.js
@@ -3,23 +3,20 @@ import { connect } from "@/utils/db";
 import User from "@/models/UserModel";
 
 export const PUT = async (req) => {
-  const user = await req?.json();
   await connect();
+  const user = await req.json();
   const exsistingUser = await User.find({ email: user.email });
 
-  try {
-    if (user && exsistingUser.length === 0) {
-      const createUser = new User({
-        name: user.name,
-        email: user.email,
-        image: user.image,
-        friends: ["sam", "shree", "sim"],
-      });
-      await createUser.save();
-      return new NextResponse("created");
-    }
-  } catch (error) {
-    console.log(error);
-    return new NextResponse(error);
+  if (user && exsistingUser.length === 0) {
+    const createUser = new User({
+      name: user.name,
+      email: user.email,
+      image: user.image,
+      friends: ["sam", "shree", "sim"],
+    });
+    await createUser.save();
+    return new NextResponse("created");
+  } else {
+    return new NextResponse("DB connection error");
   }
 };
